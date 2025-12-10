b4faff1b94a56a0eb6dc04f8d3fa584bb95929f0
April <60031800+aprilrr@users.noreply.github.com>
2025-12-10 21:16:25 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/blockrouter/main.go
@@ -0,0 +1,23 @@
+// cmd/blockrouter/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "blockrouter/internal/blockrouter"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := blockrouter.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
